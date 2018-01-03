5d24c5c74946bb6a73e4d33d816c0e61f7eb9f76
Aray Ibrayeva <ibrayeva.aray@bk.ru>
2018-01-03 16:36:23 +0600

    5.4 in test/java/RestTest.java

--- src/test/java/RestTest.java
+++ src/test/java/RestTest.java
@@ -3,7 +3,9 @@ import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
 import static io.restassured.RestAssured.given;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -14,13 +16,13 @@ public class RestTest {
         RestAssured.baseURI = "http://jsonplaceholder.typicode.com";
     }
 
-    @Test(enabled=false)
+    @Test
     public void checkStatusCode() {
         Response response = given().get("/users").andReturn();
         Assert.assertEquals(response.getStatusCode(), 200);
     }
 
-    @Test(enabled=false)
+    @Test
     public void checkHeader() {
         Response response = given().get("/users").andReturn();
         Assert.assertEquals(response.getHeader("Content-Type"), "application/json; charset=utf-8");
